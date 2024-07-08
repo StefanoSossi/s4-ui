304636a45cddff5c01ad9a477422b6c58ee01356
Stefano Sossi <stefano.sossi@truextend.com>
2024-07-08 11:56:45 -0400

    [feature/s4-3]: Added store and redux toolkit

--- src/store/state/state.js
+++ src/store/state/state.js
@@ -1,13 +1,13 @@
-import { createStore, applyMiddleware, combineReducers } from "redux";
-import thunk from "redux-thunk";
-import categoryReducer from "../reducers/class.reducer";
+import { combineReducers } from "redux";
+import classReducer from "../reducers/class.reducer";
 import studentReducer from "../reducers/student.reducer";
+import { configureStore } from "@reduxjs/toolkit";
 
 const rootReducer = combineReducers({
-	category: categoryReducer,
+	class: classReducer,
 	student: studentReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = configureStore({ reducer: rootReducer });
 
 export default store;
