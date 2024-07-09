8962e70aa1571076ae901d5fa5bd0c4c02c85467
Stefano Sossi <stefano.sossi@truextend.com>
2024-07-09 16:42:35 -0400

    [feature/s4-4]: Added students items

--- src/components/RowClass/row.class.component.jsx
+++ src/components/RowClass/row.class.component.jsx
@@ -5,38 +5,54 @@ import Typography from "@mui/material/Typography";
 import EditTwoToneIcon from "@mui/icons-material/EditTwoTone";
 import DeleteForeverTwoToneIcon from "@mui/icons-material/DeleteForeverTwoTone";
 import VisibilityTwoToneIcon from "@mui/icons-material/VisibilityTwoTone";
+import StudentItemComponent from "../StudentItem/student.item.component";
 
 function RowClassComponent({ classItem }) {
-	const [openMenu, setOpenMenu] = useState(false);
+	const [openMore, setOpenMore] = useState(false);
 
 	return (
-		<div className="class-row">
-			<div className="class-code">
-				<Typography variant="body1" component="div">
-					{classItem.code}
-				</Typography>
+		<>
+			<div className="class-row">
+				<div className="class-code">
+					<Typography variant="body1" component="div">
+						{classItem.code}
+					</Typography>
+				</div>
+				<div className="class-title">
+					<Typography variant="body1" component="div">
+						{classItem.title}
+					</Typography>
+				</div>
+				<div className="class-description">
+					<Typography variant="body1" component="div">
+						{classItem.description}
+					</Typography>
+				</div>
+				<div className="class-students-number">
+					<Typography variant="body1" component="div">
+						{classItem.students.length}
+					</Typography>
+				</div>
+				<div className="class-actions">
+					<VisibilityTwoToneIcon onClick={() => setOpenMore(!openMore)} />
+					<EditTwoToneIcon />
+					<DeleteForeverTwoToneIcon />
+				</div>
 			</div>
-			<div className="class-title">
-				<Typography variant="body1" component="div">
-					{classItem.title}
-				</Typography>
-			</div>
-			<div className="class-description">
-				<Typography variant="body1" component="div">
-					{classItem.description}
-				</Typography>
-			</div>
-			<div className="class-students-number">
-				<Typography variant="body1" component="div">
-					{classItem.students.length}
-				</Typography>
-			</div>
-			<div className="class-actions">
-				<VisibilityTwoToneIcon />
-				<EditTwoToneIcon />
-				<DeleteForeverTwoToneIcon />
-			</div>
-		</div>
+			{openMore ? (
+				<div className="class-expanded-row">
+					{classItem.students.map((student) => (
+						<StudentItemComponent
+							key={student.id}
+							student={student}
+							classId={classItem.id}
+						></StudentItemComponent>
+					))}
+				</div>
+			) : (
+				<></>
+			)}
+		</>
 	);
 }
 
---
+++ src/components/StudentItem/student.item.component.jsx
@@ -0,0 +1,73 @@
+import "./student.item.style.css";
+import React, { useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import { useDispatch } from "react-redux";
+import { removeStudentOfClass } from "../../store/slicers/class.slice";
+
+const SATURATION = 65;
+const getColorFromString = (string = "") => {
+	const lightness = 70;
+	const hue = _getHueFromString(string);
+	return `hsl(${hue},${SATURATION}%,${lightness}%)`;
+};
+const getFontColorFromString = (string = "") => {
+	const lightness = 32;
+	const hue = _getHueFromString(string);
+	return `hsl(${hue},${SATURATION}%,${lightness}%)`;
+};
+
+const _getHueFromString = (string = "") => {
+	let hash = 0;
+	for (let i = 0; i < string.length; i++) {
+		hash = string.charCodeAt(i) + ((hash << 5) - hash);
+	}
+	return Math.abs(hash % 360);
+};
+
+function StudentItemComponent({ student, classId }) {
+	const dispatch = useDispatch();
+
+	const studentAcronyms = (
+		student.firstName.substring(0, 1) + student.lastName.substring(0, 1)
+	).toUpperCase();
+	const fullName = student.firstName + " " + student.lastName;
+	const backgroundColor = getColorFromString(fullName);
+	const fontColor = getFontColorFromString(fullName);
+
+	const handleRemoveStudent = () => {
+		dispatch(removeStudentOfClass({ classId, studentId: student.id }));
+	};
+
+	return (
+		<>
+			<div className="student-item">
+				<IconButton
+					className="remove-student-button"
+					onClick={handleRemoveStudent}
+				>
+					<CloseIcon />
+				</IconButton>
+				<Avatar
+					sx={{
+						backgroundColor,
+						color: fontColor,
+						width: 45,
+						height: 45,
+					}}
+				>
+					{studentAcronyms}
+				</Avatar>
+				<div className="student-name">
+					<Typography variant="body1" component="div">
+						{fullName}
+					</Typography>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default StudentItemComponent;
